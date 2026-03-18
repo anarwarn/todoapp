c01646af848072b3bdb599fc9c0b618f113be58b
Anar Velizade <welizadeanar734@gmail.com>
2026-03-18 19:55:10 +0000

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req , res) => {res.send('hello')})
+
+app.listen(PORT, () => {console.log('server is running on port ${PORT}')});
