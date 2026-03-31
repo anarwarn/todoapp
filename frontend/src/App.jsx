87073105009c78f4d44206b8fb04afc90e926f70
Anar Velizade <velizadeanar2006@gmail.com>
2026-03-31 16:41:55 +0100

    fixed problem: when typing some invalid url it will be redirected to login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import LoginPage from './pages/loginpage.jsx'
 import SignupPage from './pages/signuppage.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   )
